5793b7e7785bb0be001c131cfadfed0274029789
DELL <DELL@DESKTOP-I5JEI3M>
2024-02-12 11:51:52 +0530

    first commit

---
+++ Project1/src/main/java/myPack/Example1.java
@@ -0,0 +1,13 @@
+package myPack;
+
+public class Example1 {
+	public void show()
+	{
+		System.out.println("Good Morning");
+	}
+
+	public static void main(String[] args) {
+		Example1 obj = new Example1();
+		obj.show();
+	}
+}
